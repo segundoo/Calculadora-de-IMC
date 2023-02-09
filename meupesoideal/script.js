eb92d5f6834901c40ef2d5de3635ebd1c14ce79a
Edilson Segundo <edilsonsegundoo@outlook.com>
2023-02-08 22:18:04 -0300

    MPI-V1.0
    primeira versão da aplicação de calculo do IMC

---
+++ meupesoideal/script.js
@@ -0,0 +1,8 @@
+function calcular() {
+    let peso = document.querySelector('inout#peso')
+    let altura = document.querySelector('input#altura')
+    if (peso.length == 0 || altura == 0) {
+        window.alert ('[ERRO] Valores invalidos! por favor digite valores maiores que 1.')
+    }
+    
+}
